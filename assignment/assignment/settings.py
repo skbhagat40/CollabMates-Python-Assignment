d88f4d8e9fa7ddbcf2771b87b7577f315a78d69a
skbhagat40 <skbhagat40@gmail.com>
2019-05-25 13:04:29 +0530

    update readme

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'assignment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_name',
+        'USER': '',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
