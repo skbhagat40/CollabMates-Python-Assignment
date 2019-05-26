682b2805b9d50ba2bb8284be862f4127b85cabfe
skbhagat40 <skbhagat40@gmail.com>
2019-05-26 11:04:23 +0530

    set thread to deamon, fixes the exception raised while quitting the server

--- assignment/api/views.py
+++ assignment/api/views.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from django.http import HttpResponse, JsonResponse
 from .paginators import StandardResultsSetPagination
@@ -11,8 +12,15 @@ from django.conf import settings
 def index(request):
     return HttpResponse("Hello World!")
 
+def start_new_thread(function):
+    def decorator(*args, **kwargs):
+        t = threading.Thread(target = function, args=args, kwargs=kwargs)
+        t.daemon = True
+        t.start()
+    return decorator
 
-def youtube_data(f_stop):
+@start_new_thread
+def youtube_data():
     ''' makes call to the youtube search api and saves the data to the database.
         Input Parametres : f_stop - Threading Event
         Output : None
@@ -20,33 +28,33 @@ def youtube_data(f_stop):
         query = ipl
         result type = videos
     '''
-    r = requests.get(
-        'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=date&publishedAfter=2013-01'
-        '-01T00%3A00%3A00Z&q=ipl&relevanceLanguage=en&safeSearch=strict&topicId=video&key=' + settings.YOUTUBE_KEY)
-    j_data = r.json()
-    search_result = j_data['items']
-    global_data = []
-    for res in search_result:
-        data_dict = {'video_id': res['id']['videoId'], 'publish_date': res['snippet']['publishedAt'],
-                     'title': res['snippet']['title'], 'description': res['snippet']['description'],
-                     'thumbnail_url': res['snippet']['thumbnails']['default']['url']}
-        global_data.append(data_dict)
-    unique_videos = []
-    for d in global_data:
-        key = d['video_id']
-        try:
-            _ = VideoData.objects.get(video_id=key)
-        except:
-            unique_videos.append(d)
-    for video in unique_videos:
-        serializer = VideoDataSerializer(data=video)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            print(serializer.errors)
-    if not f_stop.is_set():
-        # call youtube_data() again in 60 seconds
-        threading.Timer(120, youtube_data, [f_stop]).start()
+    while True:
+        print('i am called')
+        r = requests.get(
+            'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=date&publishedAfter=2013-01'
+            '-01T00%3A00%3A00Z&q=ipl&relevanceLanguage=en&safeSearch=strict&topicId=video&key=' + settings.YOUTUBE_KEY)
+        j_data = r.json()
+        search_result = j_data['items']
+        global_data = []
+        for res in search_result:
+            data_dict = {'video_id': res['id']['videoId'], 'publish_date': res['snippet']['publishedAt'],
+                         'title': res['snippet']['title'], 'description': res['snippet']['description'],
+                         'thumbnail_url': res['snippet']['thumbnails']['default']['url']}
+            global_data.append(data_dict)
+        unique_videos = []
+        for d in global_data:
+            key = d['video_id']
+            try:
+                _ = VideoData.objects.get(video_id=key)
+            except:
+                unique_videos.append(d)
+        for video in unique_videos:
+            serializer = VideoDataSerializer(data=video)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                print(serializer.errors)
+        time.sleep(10)
     return None
 
 
@@ -57,5 +65,5 @@ class VideosList(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     pagination_class = StandardResultsSetPagination
 
-f_stop = threading.Event()
-youtube_data(f_stop)
+
+youtube_data()
